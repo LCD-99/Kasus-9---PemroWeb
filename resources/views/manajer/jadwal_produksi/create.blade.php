0b28ed9ab0112fcf11c7eca621c0b1fa8752057c
LCD-99 <agita maulana23@gmail.com>
2024-12-04 12:51:54 +0700

    modified:   resources/views/manajer/jadwal_produksi/create.blade.php

--- resources/views/manajer/jadwal_produksi/create.blade.php
+++ resources/views/manajer/jadwal_produksi/create.blade.php
@@ -10,23 +10,23 @@
         <h1>Tambah Jadwal Produksi</h1>
 
         <form action="{{ route('manajer.jadwal_produksi.store') }}" method="POST">
-    @csrf
-    <div>
-        <label for="tanggal">Tanggal</label>
-        <input type="date" id="tanggal" name="tanggal" required>
+            @csrf
+            <div>
+                <label for="tanggal">Tanggal</label>
+                <input type="date" id="tanggal" name="tanggal" required>
+            </div>
+            <div>
+                <label for="produk_id">Produk</label>
+                <select name="produk_id" id="produk_id" required>
+                    @foreach($produks as $produk)
+                        <option value="{{ $produk->id }}">{{ $produk->nama }}</option>
+                    @endforeach
+                </select>
     </div>
-    <div>
-        <label for="produk_id">Produk</label>
-        <select name="produk_id" id="produk_id" required>
-            @foreach($produks as $produk)
-                <option value="{{ $produk->id }}">{{ $produk->nama }}</option>
-            @endforeach
-        </select>
-    </div>x
-    <div>
-        <label for="jumlah">Jumlah</label>
-        <input type="number" id="jumlah" name="jumlah" required>
-    </div>
+            <div>
+                <label for="jumlah">Jumlah</label>
+                <input type="number" id="jumlah" name="jumlah" required>
+            </div>
 
     <br><br>
     <button type="submit">Simpan</button>
